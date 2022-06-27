74021cb23b43e1415dc71a77399af4a2c6157ee5
LastBrainCells <sahilshethdevelopment@gmail.com>
2022-06-27 11:14:34 -0500

    Testing purposes

--- script.js
+++ script.js
@@ -18,14 +18,9 @@
 
      let hours = document.getElementById('hours');
      let minutes = document.getElementById('minutes');
-     let seconds = document.getElementById('seconds');
-     //let ampm = document.getElementById('ampm');
 
      let h = new Date().getHours();
      let m = new Date().getMinutes();
-     let s = new Date().getSeconds();
-
-    // let am = h >= 12 ? "PM" : "AM";
 
      if(h > 12){
          h = h - 12;
@@ -33,12 +28,9 @@
 
      h = (h < 10) ? "0" + h : h
      m = (m < 10) ? "0" + m : m
-     s = (s < 10) ? "0" + s : s
 
      hours.innerHTML = h;
      minutes.innerHTML = m;
-     seconds.innerHTML = s;
-     //ampm.innerHTML = am;
 
      var dateInfo = new Date();
 
@@ -47,25 +39,32 @@
 
      day = dateInfo.getDate();
 
-     function numberSuffix(Number){
-         var suffix;
-         if(Number == 1 || Number == 21 || Number == 31){
-             suffix = "st";
-         }
-         else if(Number == 2 || Number == 22){
-             suffix = "nd";
-         } 
-         else if(Number == 3 || Number == 23){
-             suffix = "th";
-         }
-         else{
-             suffix = "th";
-         }
-
-         return suffix;
-     }
-
-     var currentDate = days[dateInfo.getDay()] + ", " + " " + day + numberSuffix(day) + " " + month[dateInfo.getMonth()] + " " + dateInfo.getFullYear()
+     var currentDate = days[dateInfo.getDay()] + ", " + " " + month[dateInfo.getMonth()] + " " + day;
 
      document.getElementsByClassName("date")[0].innerHTML = currentDate;
  })
+
+ //Google Search Bar
+
+ $(document).ready(function(){
+    $("#search").focus(function() {
+      $(".search-box").addClass("border-searching");
+      $(".search-icon").addClass("si-rotate");
+    });
+    $("#search").blur(function() {
+      $(".search-box").removeClass("border-searching");
+      $(".search-icon").removeClass("si-rotate");
+    });
+    $("#search").keyup(function() {
+        if($(this).val().length > 0) {
+          $(".go-icon").addClass("go-in");
+        }
+        else {
+          $(".go-icon").removeClass("go-in");
+        }
+    });
+    $(".go-icon").click(function(){
+      $(".search-form").submit();
+    });
+});
+
