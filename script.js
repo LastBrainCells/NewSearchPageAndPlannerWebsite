a841b191f995a0bc1230c1b8ee3c9ff741c186c2
LastBrainCells <sahilshethdevelopment@gmail.com>
2022-06-28 19:52:04 -0500

    Added Responsive Nav Bar

--- script.js
+++ script.js
@@ -44,27 +44,14 @@
      document.getElementsByClassName("date")[0].innerHTML = currentDate;
  })
 
- //Google Search Bar
+ // Nav Bar
 
- $(document).ready(function(){
-    $("#search").focus(function() {
-      $(".search-box").addClass("border-searching");
-      $(".search-icon").addClass("si-rotate");
-    });
-    $("#search").blur(function() {
-      $(".search-box").removeClass("border-searching");
-      $(".search-icon").removeClass("si-rotate");
-    });
-    $("#search").keyup(function() {
-        if($(this).val().length > 0) {
-          $(".go-icon").addClass("go-in");
-        }
-        else {
-          $(".go-icon").removeClass("go-in");
-        }
-    });
-    $(".go-icon").click(function(){
-      $(".search-form").submit();
-    });
+ $('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
 });
+
 
